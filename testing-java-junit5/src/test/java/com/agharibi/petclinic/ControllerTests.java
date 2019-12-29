31537585bfa809ec2ec1752b7ad91a1d81dd6856
Armen G <gharibi.armen@yahoo.com>
2019-12-29 08:50:54 -0800

    Added common functionality default test methods

--- testing-java-junit5/src/test/java/com/agharibi/petclinic/ControllerTests.java
+++ testing-java-junit5/src/test/java/com/agharibi/petclinic/ControllerTests.java
@@ -1,8 +1,15 @@
 package com.agharibi.petclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("controllers")
 public interface ControllerTests {
 
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("Let's do something here");
+    }
 }
