1e1ae954d4c4cdfa5a01ebb8b0b4fa9fab62eb25
Armen G <gharibi.armen@yahoo.com>
2019-12-29 10:59:26 -0800

    Added junit extentions

---
+++ testing-java-junit5/src/test/java/com/agharibi/petclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -0,0 +1,36 @@
+package com.agharibi.petclinic.services.springdatajpa;
+
+import com.agharibi.petclinic.junitextentions.TimingExecution;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(TimingExecution.class)
+class PetTypeSDJpaServiceIT {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
